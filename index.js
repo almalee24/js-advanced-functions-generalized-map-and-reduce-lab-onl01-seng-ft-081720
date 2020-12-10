b69e5ee6d8b6baa86b23f00252ab8dd46e2780b7
almalee24 <flores.almalee@gmail.com>
2020-12-10 12:40:42 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(arr, func){
+    let final= []
+    arr.forEach(element => {
+        final.push(func(element))
+    });
+    return final
+}
+
+function reduce(arr, func, start){
+    let b = (!!start) ? start : arr[0];
+    let i = (!!start) ? 0 : 1
+
+    for(; i < arr.length; i++){
+        b = func(arr[i], b)
+    }
+    return b
+}
